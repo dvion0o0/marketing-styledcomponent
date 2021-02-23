9f06b5b35e31a559e5759441995645a20900c121
Dv <dv@Dvs-MacBook-Pro.local>
2021-02-24 02:01:03 +0530

    scrolladded

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { Navbar, Footer } from "./components";
 import Home from "./pages/HomePage/Home";
 import Products from "./pages/Products/Products";
 import Services from "./pages/Services/Services";
+import ScrollToTop from "./components/ScrollToTop";
 function App() {
   return (
     <Router>
       <GlobalStyle />
+      <ScrollToTop />
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
